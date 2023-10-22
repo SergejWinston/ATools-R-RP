2e640c4d272d6e9805fbcb1ec3047051fb05e714
Sergej Nekrasov <sergey.winston@bk.ru>
2023-10-22 20:48:10 +0300

    Upload library

---
+++ moonloader/luarocks/lua/luarocks/core/hardcoded.lua
@@ -0,0 +1,13 @@
+return {
+	SYSCONFDIR = [[C:\Program Files (x86)\RADMIR LAUNCHER\resources\projects\crmp\moonloader\luarocks]],
+	SYSTEM = [[windows]],
+	FORCE_CONFIG = true,
+	LUA_INCDIR = [[C:\Program Files (x86)\RADMIR LAUNCHER\resources\projects\crmp\moonloader\luajit\inc]],
+	LUA_LIBDIR = [[C:\Program Files (x86)\RADMIR LAUNCHER\resources\projects\crmp\moonloader\luajit\lib]],
+	LUA_DIR = [[C:\Program Files (x86)\RADMIR LAUNCHER\resources\projects\crmp\moonloader\luajit]],
+	LUA_INTERPRETER = [[luajit.exe]],
+	WIN_TOOLS = [[C:\Program Files (x86)\RADMIR LAUNCHER\resources\projects\crmp\moonloader\luarocks\tools]],
+	LUA_BINDIR = [[C:\Program Files (x86)\RADMIR LAUNCHER\resources\projects\crmp\moonloader\luajit\bin]],
+	PROCESSOR = [[x86]],
+	PREFIX = [[C:\Program Files (x86)\RADMIR LAUNCHER\resources\projects\crmp\moonloader\luarocks]],
+}
